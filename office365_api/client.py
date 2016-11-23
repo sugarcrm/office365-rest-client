ecdae5d9fac7f3fd72dd35999e0f1e216540cfd0
aldwyn <aldwyn.up@gmail.com>
2016-11-23 17:44:47 +0800

    Removed user on client

--- office365_api/client.py
+++ office365_api/client.py
@@ -10,19 +10,18 @@ class Office365Client(object):
 
     def __init__(self, client_id, client_secret,
                  redirect_uri, access_token, refresh_token,
-                 user=None, credentials_backend=DefaultCredentialsBackend):
+                 credentials_backend=DefaultCredentialsBackend):
         self.client_id = client_id
         self.client_secret = client_secret
         self.redirect_uri = redirect_uri
         self.access_token = access_token
         self.refresh_token = refresh_token
-        self.user = user
         self.credentials_backend = credentials_backend
         self.outlook = OutlookService(self)
         self.calendar = CalendarService(self)
         self.token = TokenService(self)
 
     def save_credentials(self, access_token, refresh_token, expires_at):
+        self.credentials_backend().save_credentials(self.access_token, access_token, refresh_token, expires_at)
         self.access_token = access_token
         self.refresh_token = refresh_token
-        self.credentials_backend().save_credentials(self.user, access_token, refresh_token, expires_at)
