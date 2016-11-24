455f3364b59c872b50a17592f8e0eac88c69cd52
aldwyn <aldwyn.up@gmail.com>
2016-11-24 13:58:25 +0800

    Removed logging

--- office365_api/services.py
+++ office365_api/services.py
@@ -44,8 +44,6 @@ class BaseAPIService(BaseService):
                                           filter_backend=filter_backend)
         while next_link:
             response = self.execute_request(next_link, custom_headers)
-            import logging
-            logging.info(response)
             result.extend(response['value'])
             next_link = response.get('@odata.nextLink')
             delta_link_qs = parse_qs(urlparse(response.get('@odata.deltaLink', '')).query)
