df9ee0483501356216153d687d67e4c9acb569e2
cmabastar <marlon.abastar@gmail.com>
2017-09-12 13:23:06 +0800

    Fix creation of object

--- office365_api/v2/client.py
+++ office365_api/v2/client.py
@@ -11,4 +11,4 @@ class MicrosoftGraphClient(object):
 
         self.users = UserServicesFactory(self)
         self.me = self.users('me')
-        self.subscription = SubscriptionFactory(self)
+        self.subscription = SubscriptionFactory()
