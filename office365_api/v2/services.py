8551a727ae2500dc36f543d07b8185483ba30d4e
cmabastar <marlon.abastar@gmail.com>
2018-04-06 11:03:19 +0800

    Added delta query for mailfolders.

--- office365_api/v2/services.py
+++ office365_api/v2/services.py
@@ -389,6 +389,32 @@ class MessageService(BaseService):
         next_link = resp.get('@odata.nextLink')
         return resp, next_link
 
+    def delta_list(self, start_datetime=None, end_datetime=None, delta_token=None, max_entries=50):
+        """
+        Support tracking of changes in the calendarview.
+
+        https://developer.microsoft.com/en-us/graph/docs/concepts/delta_query_overview
+        """
+        path = '/messages/delta'
+
+        method = 'get'
+        headers = {
+            'Prefer': 'odata.maxpagesize=%d' % max_entries
+        }
+        if not delta_token:
+            query_params = {
+                'startDateTime': start_datetime,
+                'endDateTime': end_datetime,
+            }
+        else:
+            query_params = {
+                '$deltaToken': delta_token,
+            }
+        resp = self.execute_request(method, path,
+                                    query_params=query_params, headers=headers)
+        next_link = resp.get('@odata.nextLink')
+        return resp, next_link
+
     def get(self, message_id, _filter=None):
         """https://graph.microsoft.io/en-us/docs/api-reference/v1.0/api/user_list_messages ."""
         path = '/messages/{}'.format(message_id)
@@ -434,6 +460,7 @@ class MessageService(BaseService):
         method = 'post'
         body = json.dumps({'DestinationId': destination_id})
         return self.execute_request(method, path, body=body)
+
 
 
 class AttachmentService(BaseService):
@@ -551,6 +578,26 @@ class MailFolderService(BaseService):
         path = '/mailFolders'
         method = 'get'
         resp = self.execute_request(method, path)
+        next_link = resp.get('@odata.nextLink')
+        return resp, next_link
+
+    def delta_list(self, folder_id, delta_token=None, max_entries=50):
+        """
+        Support tracking of changes in the mailFolders.
+
+        https://developer.microsoft.com/en-us/graph/docs/concepts/delta_query_overview
+        """
+        path = '/mailFolders/{}/messages/delta'.format(folder_id)
+
+        method = 'get'
+        headers = {
+            'Prefer': 'odata.maxpagesize=%d' % max_entries
+        }
+        query_params = {
+            '$deltaToken': delta_token,
+        }
+        resp = self.execute_request(method, path,
+                                    query_params=query_params, headers=headers)
         next_link = resp.get('@odata.nextLink')
         return resp, next_link
 
