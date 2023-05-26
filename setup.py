d8edf24eb5d1e835be4512a6c38cad93c09b4aa1
Sergey Mihaylishin <smihaylishin@sugarcrm.com>
2023-05-26 11:25:25 +0200

    CSC-4351 RAW Email

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 
 
 setup(name='office365-rest-client',
-      version='3.2.1',
-      description='Python api wrapper for Office365 API v3.2.1',
+      version='3.2.2',
+      description='Python api wrapper for Office365 API v3.2.2',
       author='SugarCRM',
       packages=find_packages(),
       install_requires=[
