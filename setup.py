38ab21abb777eb7a1582910af1105d8b40b459dc
Sergey Mihaylishin <smihaylishin@sugarcrm.com>
2024-11-27 16:43:10 +0100

    CSC-6138 Migrate auth to new libraries

--- setup.py
+++ setup.py
@@ -2,12 +2,8 @@
 from setuptools import find_packages, setup
 
 setup(name='office365-rest-client',
-      version='3.3.5',
-      description='Python api wrapper for Office365 API v3.3.5',
+      version='3.4.0',
+      description='Python api wrapper for Office365 API v3.4.0',
       author='SugarCRM',
       packages=find_packages(),
-      install_requires=[
-          'oauth2client>=4.0.0',
-          'requests>=2.31.0',
-      ],
       zip_safe=False)
