956fb4231c61be030e0951fdced5c542dd97d18d
Sergey Mihaylishin <smihaylishin@sugarcrm.com>
2025-02-10 13:45:50 +0100

    CSC-6369 Outlook Update/Delete contact sync doesn't work

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 from setuptools import find_packages, setup
 
 setup(name='office365-rest-client',
-      version='3.4.0',
-      description='Python api wrapper for Office365 API v3.4.0',
+      version='3.4.1',
+      description='Python api wrapper for Office365 API v3.4.1',
       author='SugarCRM',
       packages=find_packages(),
       zip_safe=False)
